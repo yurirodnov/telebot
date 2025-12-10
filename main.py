8ce009c1fce4a9070890b08aac9e7b25b278c4e4
yuri_rodnov <somewaystodo@yandex.ru>
2025-12-11 00:18:49 +0300

    add me handler

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from aiogram import Bot, Dispatcher, Router, F
 from aiogram.fsm.storage.memory import MemoryStorage
 from aiogram.filters import Command
 from aiogram.types import Message
-import messages.messages
+import texts.texts
 import asyncio
 import config
 import logging
@@ -16,7 +16,10 @@ router = Router()
 @dp.message(Command("start"))
 async def command_start_handler(message: Message) -> None:
     await message.answer("Started!")
-
+    
+@dp.message(Command("me"))
+async def command_me_handler(message: Message) -> None:
+    await message.answer(f"Ты {message.from_user.id}")
     
 @dp.message(F.text == 'Юра')
 async def message_filter_handler(message:Message):
